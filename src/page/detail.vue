8bc8c734971359e1df0c2f591cfe91e2c25c18cc
wxh_leo <754557634@qq.com>
2019-01-09 14:40:19 +0800

    添加详情页面

--- src/page/detail.vue
+++ src/page/detail.vue
@@ -24,6 +24,28 @@
       <p class="title">【福州】身为福建省会似乎显得不那么有存在感。</p>
       <p>福州，身为福建省会，在厦门的耀眼光芒之下，似乎显得不那么有存在感。曾在这座城市读完大学，整整生活了四年，对它的印象，除去永远杂乱无章、永远在修天桥、修地铁、修路的紊乱交通，除去商业化透了的三坊七巷，当然就是鱼丸、肉燕、芋泥、花生汤、锅边糊、元宵………这些说不完的街边小食，以及蹄髈破店的蹄髈，邓记饭庄的荔枝肉.....这些苍蝇馆子里的地道福州菜。</p>
     </div>
+    <div class="comment">
+      <div class="title">
+        <div>评论</div>
+        <div>12条</div>
+      </div>
+      <div class="comment-list">
+        <div class="list-block" v-for="(item,index) in comment_list" :key="index">
+          <div class="info">
+            <div>{{item.name}}</div>
+            <div>{{item.date}}</div>
+          </div>
+          <div class="comment-content">{{item.content}}</div>
+        </div>
+      </div>
+      <div class="btns">
+        <button>更多评论</button>
+      </div>
+    </div>
+    <div class="add-comment">
+      <img src="../assets/header.jpg">
+      <input v-model="comment" placeholder="添加评论...">
+    </div>
   </div>
 </template>
 
@@ -34,6 +56,7 @@ export default {
   components: {Swiper, Slide, myHeader},
   data () {
     return {
+      comment: '',
       title: '笔记内容',
       list: [
         {
@@ -44,6 +67,13 @@ export default {
         },
         {
           url: require('../assets/banner/third.jpg')
+        }
+      ],
+      comment_list: [
+        {
+          name: '用户123',
+          date: '2019-01-02',
+          content: '说的真好'
         }
       ]
     }
