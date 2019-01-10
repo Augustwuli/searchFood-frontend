f461862f1fefd2dff9e33c2b410a99568bd99647
wxh_leo <754557634@qq.com>
2019-01-10 22:39:18 +0800

    添加搜索结果

--- src/components/search.vue
+++ src/components/search.vue
@@ -3,18 +3,27 @@
     <img src="../assets/position.png" @click="click">
     <div class="form-search">
       <img class="search-img" src="../assets/search.png">
-      <input type="text" placeholder="搜索美食笔记">
+      <input type="text" placeholder="搜索美食笔记"  @change="getSearch" v-model="search">
     </div>
     <img src="../assets/info.png">
   </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      search: ''
+    }
+  },
   methods: {
     click () {
       this.$router.push({
         path: '/position',
         name: 'Position'
+      })
+    },
+    getSearch () {
+      this.$api.get(null, `/notes/search/${this.search}`, null, r => {
       })
     }
   }
